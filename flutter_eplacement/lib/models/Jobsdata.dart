40f4d6ec41f98288c07b6c7b659c8d28d2ee594f
faliyaaakash <aakashfaliya1@gmail.com>
2026-05-19 12:40:44 +0530

    "listing screen commited"

---
+++ flutter_eplacement/lib/models/Jobsdata.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class JobData {
+  String name;
+  String logo;
+  String city;
+  String role;
+  String eligibility;
+  String stipend;
+  String applylink;
+
+  JobData({required this.name, required this.city , required this.role,required this.logo,required this.eligibility ,required this.stipend ,required this.applylink});
+  
+}
