5918bda9497b33e664dec6ff14a9439df4ae15bf
faliyaaakash <aakashfaliya1@gmail.com>
2026-05-21 10:58:04 +0530

    all other updates completed

--- flutter_eplacement/lib/widgets/fragment_holder.dart
+++ flutter_eplacement/lib/widgets/fragment_holder.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_eplacement/models/Jobsdata.dart';
 import 'package:flutter_eplacement/widgets/listingScreen.dart';
 
-
 class FragmentHolder extends StatefulWidget {
   const FragmentHolder({super.key});
 
